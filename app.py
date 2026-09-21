54524afd447b7c9cfa3df8ce88b088c0080a98c2
Neha Zari <nehazari61@gmail.com>
2026-09-21 16:05:41 +0500

    Make Gmail import optional

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os, re, json, secrets
 from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
 from pathlib import Path
-from gmail import send_email
-
+try:
+    from gmail import send_email
+except ImportError:
+    send_email = None
 import pandas as pd
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify, redirect, make_response
